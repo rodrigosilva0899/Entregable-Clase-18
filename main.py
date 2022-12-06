4af8acea894f9080c8cc126461ae15e80a4220bf
Rodrigo Silva <rodrigosilva0899@gmail.com>
2022-12-06 11:43:44 +0000

    Agrego project MVT

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
 print("gol")
-
-# 1) Git init
-# 2) git config --global user.name "Rodrigo Silva"
-# 3) git config --global user.email "rodrigosilva0899@gmail.com"
-# 4) git config --list -> para ver que la configuración este ok
-# 5) git remote add origin "link" -> para configurar GITHUB -> GITHUB / Crear Proyecto / Code / HTTPS / copio link
-# 6) git branch -> para chequear que estemos en main
-# 7) git pull -> para traer lo del repositorio
-# 8) git add main.py -> para versionar el archivo main.py
-# 9) git status -> para chequear que hay un nuevo archivo para seguir
-# 10) git commit -m -> para commitear el archivo
-# 11) git push origin main -> para enviar los cambios a github
