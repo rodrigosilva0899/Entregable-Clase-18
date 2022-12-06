cd9f613f59bd442ebfb358d63bdd499ec379a0e7
Rodrigo Silva <rodrigosilva0899@gmail.com>
2022-12-06 19:34:00 +0000

    Ultimos cambios - funciona, falta la fecha

--- seed_data.py
+++ seed_data.py
@@ -1,7 +1,7 @@
 from MVT.models import Familiar
 
 Familiar(nombre="Tomas", direccion="Libertador 4200", numero_dni=41300100).save()
-Familiar(nombre="Tomas", direccion="Libertador 4200", numero_dni=41300100).save()
-Familiar(nombre="Tomas", direccion="Libertador 4200", numero_dni=41300100).save()
+Familiar(nombre="Nicolas", direccion="Libertador 4300", numero_dni=42000399).save()
+Familiar(nombre="Santiago", direccion="Libertador 4400", numero_dni=20393844).save()
 
 print("Se cargo con éxito los usuarios de pruebas")
