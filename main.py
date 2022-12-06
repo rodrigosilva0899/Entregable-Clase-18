c1d9a41b4072b5eb74476ed4c277a2ebbe7ef20d
Rodrigo Silva <rodrigosilva0899@gmail.com>
2022-12-06 11:08:05 +0000

    Agrego archivo main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("gol")
+
+# 1) Git init
+# 2) git config --global user.name "Rodrigo Silva"
+# 3) git config --global user.email "rodrigosilva0899@gmail.com"
+# 4) git config --list -> para ver que la configuración este ok
+# 5) git remote add origin "link" -> para configurar GITHUB -> GITHUB / Crear Proyecto / Code / HTTPS / copio link
+# 6) git branch -> para chequear que estemos en main
+# 7) git pull -> para traer lo del repositorio
